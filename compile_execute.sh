71a30b3bd3b70bd8c032bfc675e2d69bac2ebd43
theSyndicate <dragon99x@hotmail.com>
2021-09-12 22:01:18 -0500

    Updated the compilation/execution script Now a Jar file is created.

--- compile_execute.sh
+++ compile_execute.sh
@@ -3,7 +3,6 @@
 # gets the current directory of the script (assumes in project directory)
 DIRECTORY=$(cd `dirname $0` && pwd)
 
-
 # removes target directory for a clean build
 rm -rf $DIRECTORY/target/
 
@@ -11,6 +10,13 @@ rm -rf $DIRECTORY/target/
 javac -d $DIRECTORY/target -classpath "$DIRECTORY/." $DIRECTORY/src/net/thesyndicate/io/*.java
 
 # runs the compiled code
-java -ea -classpath "$DIRECTORY/target/." net.thesyndicate.io.Multicast
+java -ea -classpath "$DIRECTORY/target/." net.thesyndicate.io.Receiver
+
+# package into a jar file
+mkdir $DIRECTORY/jar
+jar -cfev $DIRECTORY/jar/Multicast.jar net.thesyndicate.io.Multicast -C $DIRECTORY/target net/thesyndicate/io/
+
+# runs the package code
+java -jar $DIRECTORY/jar/Multicast.jar
 
 #javac -d target -classpath ".:lib/*" src/*.java && java -classpath "target/.:lib/*" Server 8080
